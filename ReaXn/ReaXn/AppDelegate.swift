ccf54862702fb1933004fd67ce162af7b1cd6f05
Benjamin Reynolds <reynoldsben94@gmail.com>
2015-07-11 14:50:34 -0700

    Added local notifications with handler (not actionable yet)

--- ReaXn/ReaXn/AppDelegate.swift
+++ ReaXn/ReaXn/AppDelegate.swift
@@ -16,6 +16,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        
+        application.registerUserNotificationSettings(UIUserNotificationSettings(forTypes: .Alert | .Badge | .Sound, categories: nil))  // types are UIUserNotificationType members
+        
         return true
     }
 
@@ -39,6 +42,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+    
+    //MARK: - Notifications
+    
+    func application(application: UIApplication, didReceiveLocalNotification notification: UILocalNotification) {
+        NSNotificationCenter.defaultCenter().postNotificationName("receivedPushNotification", object: self)
     }
 
 
--- ReaXn/ReaXn/ViewController.swift
+++ ReaXn/ReaXn/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +19,38 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "handlePushNotification:", name: "receivedPushNotification", object: nil)
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: "receivedPushNotification", object: nil)
+    }
 
+    @IBAction func buttonPressed(sender: UIButton) {
+        let timeDelayInSeconds = NSTimeInterval(15)
+        println("Scheduling a notification in \(timeDelayInSeconds) seconds...")
+        
+        // create a corresponding local notification
+        var notification = UILocalNotification()
+        notification.alertBody = "Notification text goes here" // text that will be displayed in the notification
+        notification.alertAction = "Action" // text that is displayed after "slide to..." on the lock screen - defaults to "slide to view"
+        notification.fireDate = NSDate().dateByAddingTimeInterval(timeDelayInSeconds) // todo item due date (when notification will be fired)
+        notification.soundName = UILocalNotificationDefaultSoundName // play default sound
+        notification.userInfo = ["UUID": NSUUID().UUIDString, ] // assign a unique identifier to the notification so that we can retrieve it later
+        notification.category = "TODO_CATEGORY"
+        UIApplication.sharedApplication().scheduleLocalNotification(notification)
+    }
+    
+    //MARK: - Notifications
+    
+    func handlePushNotification(notification : NSNotification) {
+        println("handle push notification")
+        println(notification)
+    }
+    
+    
 
 }
 
